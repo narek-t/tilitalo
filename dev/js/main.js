c0a5ee0865ee628c513831bca369e824b437f404
Narek-T <narek.tarverdyan@bk.ru>
2016-12-17 21:41:12 +0300

    init

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,1 +1,17 @@
-console.log('hello world');
+$(document).ready(function() {
+	$('.menu-toggle').click(function(event) {
+		event.preventDefault();
+		$(this).toggleClass('active');
+		$('.menu').toggleClass('visible');
+	});
+
+	$('.services__list .heading').click(function(event) {
+		var parentLi = $(this).parents('li');
+		if(parentLi.hasClass('active')) {
+			parentLi.removeClass('active');
+		}else {
+			$('.services__list li').removeClass('active');
+			parentLi.addClass('active');
+		}
+	});
+});
