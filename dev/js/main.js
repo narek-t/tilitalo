9053fe89f2af4d62c43e8989071f3e04c2e2b30e
Narek-T <narek.tarverdyan@bk.ru>
2016-12-25 13:35:19 +0300

    detail page

--- dev/js/main.js
+++ dev/js/main.js
@@ -87,4 +87,18 @@ $(document).ready(function() {
 			}
 		}
 	});
+
+	$('.detail-slider').slick({
+		infinite: true,
+		slidesToShow: 1,
+		// responsive: [
+	 //    {
+	 //      breakpoint: 1000,
+	 //      settings: {
+	 //        slidesToShow: 1,
+	 //        slidesToScroll: 1,
+	 //      }
+	 //    },
+	 //  ]
+	});
 });
